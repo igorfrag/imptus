f45173529b63e9dc35d3dc67b29df006c39f3615
igorfrag <igorfragoso3@gmail.com>
2025-05-27 16:21:07 -0300

    fix: route header/footer on layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
+import Footer from '@/shared/Footer/Footer';
+import Header from '@/shared/Header/Header';
 import '../styles/globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
       <body>
+        <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
