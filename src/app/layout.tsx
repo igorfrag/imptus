bee978ebeb6f28eb19d3bb463283f072c165c48b
lara123-pg <lamorimalvescavalcante@gmail.com>
2025-05-28 15:20:25 -0300

    feat:update header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import Footer from '@/shared/Footer/Footer';
 import Header from '@/shared/Header/Header';
 import '../styles/globals.css';
+import { Manrope } from 'next/font/google'
 
+const getManrope = Manrope({
+  subsets: ['latin'],
+  variable: '--font-manrope',
+  weight: ['400', '500', '600', '700']
+})
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={getManrope.variable}>
       <body>
         <Header />
         <main>{children}</main>
--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -3,38 +3,34 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="flex justify-around items-center gap-10 py-8">
+    <header className="flex justify-around items-center gap-10 py-3 px-4">
         <div>
-          <Image src={'/next.svg'} width={100} height={100} alt="Next Logo" />
+          <Image src={'/dark.png'} width={100} height={100} alt="Landify Logo" />
         </div>
         
 				<nav>
-					<ul className="flex gap-15">
-						<li className="border border-primary p-2 rounded-md">
-							<Link href="#">Home</Link>
+					<ul className="flex gap-8 font-bold text-sm">
+						<li>
+							<Link href="#">About</Link>
 						</li>
-						<li className="border border-primary p-2 rounded-md">
-							<Link href="#">Sobre</Link>
+						<li>
+							<Link href="#">Product</Link>
 						</li>
-						<li className="border border-secondary p-2 rounded-md">
-							<Link href="#">Serviços</Link>
+						<li>
+							<Link href="#">Pricing</Link>
 						</li>
-            <li className="border border-primary p-2 rounded-md">
-              <Link href="#">Depoimentos</Link>
+            <li>
+              <Link href="#">Resources</Link>
             </li>
-            <li className="border border-primary p-2 rounded-md">
-              <Link href="#">Contato</Link>
+            <li>
+              <Link href="#">Jobs</Link>
             </li>
 					</ul>
 				</nav>
 
         <div className="flex gap-10">
-          <button type="button" className="py-2 px-6 border border-secondary rounded-md">
-            <Link href="#">Login</Link>
-          </button>
-
-          <button type="button" className="py-2 px-6 border border-primary p-2 rounded-md">
-            <Link href="#">Cadastrar</Link>
+          <button type="button" className="min-w-[100px] h-10 bg-landingPageButton text-white font-bold text-sm rounded-lg cursor-pointer">
+            Buy now
           </button>
         </div>
 			</header>
