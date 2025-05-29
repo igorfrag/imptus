81ce84147cb2173529e520f17e27da293c72b19a
igorfrag <igorfragoso3@gmail.com>
2025-05-29 15:42:05 -0300

    fix: convert hero image to .webp from .jpg

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen max-sm:p-5  text-white bg-[url('/assets/heroimage.jpg')] bg-cover bg-center max-w-screen max-h-150 max-md:max-h-110 max-sm:max-h-96">
+    <div className="flex flex-col items-center justify-center h-screen max-sm:p-5  text-white bg-[url('/assets/heroimage.webp')] bg-cover bg-center max-w-screen max-h-150 max-md:max-h-110 max-sm:max-h-96">
       <div className="text-center max-w-3xl ">
         <h1 className="text-7xl max-sm:text-3xl  max-md:text-5xl">
           Seu limite é nosso
