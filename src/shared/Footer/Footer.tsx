627a523cdd48415989e2b5ecc455865d29d27433
igorfrag <igorfragoso3@gmail.com>
2025-05-29 15:05:46 -0300

    Fix: Footer responsiveness/acessibility

--- src/shared/Footer/Footer.tsx
+++ src/shared/Footer/Footer.tsx
@@ -3,27 +3,27 @@ import footerItems from './footerItems';
 
 const Footer = () => {
   return (
-    <footer className="flex bg-primary text-white w-full p-4 text-center justify-evenly align-center flex-row gap-4">
-      <ul className="flex flex-row  gap-5 align-center justify-center">
+    <footer className="flex bg-primary text-white  w-full p-4 text-center justify-evenly align-center flex-row gap-4">
+      <ul className="flex flex-row align-center justify-center max-md:hidden gap-6">
         {footerItems.map((section, index) => (
           <li key={index}>
-            <a href={section.link}>
+            <a href={section.link} aria-label={section.title}>
               <h2 className="text-xl  opacity-80 hover:opacity-100">{section.title}</h2>
             </a>
           </li>
         ))}
       </ul>
       <div>
-        <span> Copyright © 2025 Imptus. Todos os direitos reservados</span>
+        <span aria-label="Copyright Imptus"> Copyright © {new Date().getFullYear()} Imptus</span>
       </div>
       <div className="flex flex-row gap-4 align-center justify-center">
-        <a href="">
+        <a href="" aria-label="Github">
           <FooterIcon type="github" />
         </a>
-        <a href="">
+        <a href="" aria-label="X">
           <FooterIcon type="x" />
         </a>
-        <a href="">
+        <a href="" aria-label="Instagram">
           <FooterIcon type="instagram" />
         </a>
       </div>
