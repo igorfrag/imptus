9f1b1889148ccae81ff249ae6e788b0304ce44f2
lara123-pg <lamorimalvescavalcante@gmail.com>
2025-05-30 17:29:54 -0300

    style: change button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,21 @@
 type ButtonProps = {
-    size: 'sm' | 'lg';
-    children: React.ReactNode
-}
+  size: 'xl' | '3xl';
+  children: React.ReactNode;
+};
 
-const Button = ({ size="sm", children }: ButtonProps) => {
-    const sizeClasses = {
-        sm: 'min-w-[120px] text-sm',       
-        lg: 'min-w-[259px] text-base'
-    };
+const Button = ({ size = 'xl', children }: ButtonProps) => {
+  const sizeClasses = {
+    'xl': 'h-[50px] min-w-[120px] text-xl',
+    '3xl': 'h-[72px] min-w-[259px] text-3xl',
+  };
 
-    return (
-        <button className={`flex items-center justify-center gap-2 bg-primary-azultecnologico font-barlow-condensed font-medium rounded-[10px] cursor-pointer ${sizeClasses[size]} hover:opacity-80`}>
-            {children}
-        </button>
-    )
-}
+  return (
+    <button
+      className={`flex items-center justify-center gap-2 bg-primary-azultecnologico border-solid border border-primary-azulescuro text-primary-azulescuro font-barlow-condensed font-medium rounded-[10px] cursor-pointer ${sizeClasses[size]} hover:opacity-80`}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
