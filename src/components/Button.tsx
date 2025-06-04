65d91caa7d316224e166efc73da64278de0d3c58
Igor Fragoso <igorfragoso3@hotmail.com>
2025-06-04 16:02:11 -0300

    styles: refactor button component + change button on header

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,38 @@
 type ButtonProps = {
   size: 'xl' | '3xl';
   children: React.ReactNode;
+  bgColor?: string;
+  borderColor?: string;
+  textColor?: string;
+  hover?: boolean;
+  hoverBg?: string;
+  hoverText?: string;
+  hoverBorder?: string;
 };
 
-const Button = ({ size = 'xl', children }: ButtonProps) => {
+const Button = ({
+  size = 'xl',
+  children,
+  bgColor = 'primary-azultecnologico',
+  borderColor = 'primary-azulescuro',
+  textColor = 'primary-azulescuro',
+  hover,
+  hoverBg,
+  hoverText,
+  hoverBorder,
+}: ButtonProps) => {
   const sizeClasses = {
-    'xl': 'h-[50px] min-w-[120px] text-xl',
+    xl: 'h-[50px] min-w-[120px] text-xl',
     '3xl': 'h-[72px] min-w-[259px] text-3xl',
   };
 
   return (
     <button
-      className={`flex items-center justify-center gap-2 bg-primary-azultecnologico border-solid border border-primary-azulescuro text-primary-azulescuro font-barlow-condensed font-medium rounded-[10px] cursor-pointer ${sizeClasses[size]} hover:opacity-80`}
+      className={`flex items-center justify-center gap-2 ${'bg-' + bgColor} border-solid border ${'border-' + borderColor} ${'text-' + textColor} 
+        ${hover && hoverBg ? 'hover:bg-' + hoverBg : ''} 
+        ${hover && hoverText ? 'hover:text-' + hoverText : ''}
+        ${hover && hoverBorder ? 'hover:border-' + hoverBorder : ''}
+        font-barlow-condensed font-medium rounded-[10px] cursor-pointer ${sizeClasses[size]} hover:opacity-80`}
     >
       {children}
     </button>
